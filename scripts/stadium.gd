746d4cf9c60755993511e05fb10a8ee769196c0e
DaCodaHUB <dannylpov@gmail.com>
2026-06-22 10:14:11 -0700

    Add physics back to the beyblades

--- scripts/stadium.gd
+++ scripts/stadium.gd
@@ -30,18 +30,20 @@ func _ready() -> void:
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			var localPos = get_local_mouse_position()
-			print("local mouse pos: ", localPos)
-			if (localPos.distance_to($CenterPoint.position) < $BoundingCircle.radius):					
+			var mouse_global = get_global_mouse_position()
+			var center_global = $CenterPoint.global_position
+			
+			var distance = mouse_global.distance_to(center_global)
+			print("Distance to center: ", distance, " | Allowed Radius: ", $BoundingCircle.radius)
+			
+			if distance < $BoundingCircle.radius:                    
 				var newBlade = beybladeScene.instantiate()
-				#newBlade.position = Vector2(
-					#localPos.position.x ,
-					#localPos.position.y
-				#)
-				newBlade.position = localPos
-				add_child(newBlade)
+				# Set position in absolute world space
+				newBlade.global_position = mouse_global
+				
+				# Add it to the main scene tree directly so it moves freely
+				get_tree().current_scene.add_child(newBlade)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
